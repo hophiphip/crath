2203d12ad43babceac5701c784bbee78aef119de
hophiphip <nighop@yandex.ru>
2021-01-06 18:20:39 +0400

    Updates for bitcommit

--- bitcommit/schnorr.go
+++ bitcommit/schnorr.go
@@ -17,25 +17,50 @@ var (
 	// SmallKeySize - small-key bit size
 	// q size
 	SmallKeySize = 160
+	// w size
+	secretKeySize = 128
+	// random size
+	// r size
+	rSize = 100
+	// e size
+	eSize = 72
 
-	zero  = big.NewInt(0)
-	one   = big.NewInt(1)
-	two   = big.NewInt(2)
-	three = big.NewInt(3)
-	four  = big.NewInt(4)
-	five  = big.NewInt(5)
-	six   = big.NewInt(6)
+	zero = big.NewInt(0)
+	one  = big.NewInt(1)
+	two  = big.NewInt(2)
 )
-
-// NOTE: this imlpementation doesn't bother about 160 bits small-key size, but uses the first found one
 
 // SchnorrContext - contains public & private values for keys
 type SchnorrContext struct {
-	q  *big.Int
+	q *big.Int
+	// necessary for calculating p
 	qp *big.Int
-	p  *big.Int
-	a  *big.Int
-	g  *big.Int
+
+	p *big.Int
+
+	// necessary for calculating g
+	a *big.Int
+	g *big.Int
+
+	y *big.Int
+
+	// private key
+	w *big.Int
+}
+
+// PublicPart  - values of public key
+type PublicPart struct {
+	p, q, g, y *big.Int
+}
+
+// PrivatePart - values of private key
+type PrivatePart struct {
+	w *big.Int
+}
+
+// RandomPart - client random number generation
+type RandomPart struct {
+	r, x *big.Int
 }
 
 // Init - define all values for context
@@ -43,7 +68,8 @@ func (ctx *SchnorrContext) Init() error {
 	// Will check whether number is a prime one with certainty:  1 - 1/2*certainty
 	certainty := 100
 
-	_q, err := primegen.Primegen(rand.Reader, KeySize)
+	// Initialize q
+	_q, err := primegen.Primegen(rand.Reader, SmallKeySize)
 	if err != nil {
 		return err
 	}
@@ -51,6 +77,7 @@ func (ctx *SchnorrContext) Init() error {
 	ctx.q.Set(_q)
 	ctx.qp.Set(one)
 
+	// Calculate p
 	for {
 		// p := 2*(q * qp) + 1
 		ctx.p.Mul(ctx.q, ctx.qp)
@@ -64,6 +91,7 @@ func (ctx *SchnorrContext) Init() error {
 		ctx.qp.Add(ctx.qp, one)
 	}
 
+	// Calculate g
 	for {
 		// (2+a) mod p
 		_a, err := primegen.Primegen(rand.Reader, SmallKeySize)
@@ -78,7 +106,7 @@ func (ctx *SchnorrContext) Init() error {
 		ga := big.NewInt(0).Sub(ctx.p, one)
 		ga.Div(ga, ctx.q)
 
-		// a^ga mod p
+		// a^ga mod p = 1
 		ctx.g.Exp(ctx.a, ga, ctx.p)
 
 		// g != 1
@@ -87,10 +115,94 @@ func (ctx *SchnorrContext) Init() error {
 		}
 	}
 
+	// Calculate w
+	for {
+		_w, err := primegen.Primegen(rand.Reader, secretKeySize)
+		if err != nil {
+			return err
+		}
+		ctx.w.Set(_w)
+
+		if ctx.w.Cmp(ctx.q) < 0 {
+			break
+		}
+	}
+
+	// Calculate y
+	_negw := big.NewInt(0).Neg(ctx.w)
+	ctx.y.Exp(ctx.g, _negw, ctx.p)
+
 	return nil
 }
 
-// Test ...
-func Test() {
+// GetPublicPart - basically returns public key
+func (ctx *SchnorrContext) GetPublicPart() *PublicPart {
+	return &PublicPart{
+		p: ctx.p,
+		q: ctx.q,
+		g: ctx.g,
+		y: ctx.y,
+	}
+}
 
+// GetPrivatePart - basically returns private key
+func (ctx *SchnorrContext) GetPrivatePart() *PrivatePart {
+	return &PrivatePart{
+		w: ctx.w,
+	}
+}
+
+// FindR - random r, generated on a client
+func (public *PublicPart) FindR() (*RandomPart, error) {
+	random := &RandomPart{
+		r: big.NewInt(0),
+		x: big.NewInt(0),
+	}
+
+	// Find r
+	for {
+		_r, err := primegen.Primegen(rand.Reader, rSize)
+		if err != nil {
+			return nil, err
+		}
+		if _r.Cmp(public.q) < 0 {
+			random.r.Set(_r)
+			break
+		}
+	}
+
+	// Find x
+	random.x.Exp(public.g, random.r, public.p)
+
+	return random, nil
+}
+
+// GenE - generates e (B response)
+func GenE() (*big.Int, error) {
+	e, err := primegen.Primegen(rand.Reader, eSize)
+	if err != nil {
+		return nil, err
+	}
+
+	return e, nil
+}
+
+// FindS - gneerates s (to send to B)
+func (ctx *SchnorrContext) FindS(e *big.Int, random *RandomPart) *big.Int {
+	s := big.NewInt(0)
+	s.Mul(ctx.w, e)
+	s.Add(s, random.r)
+	s.Mod(s, ctx.q)
+
+	return s
+}
+
+// IsApproved - B approve function
+func IsApproved(x, s, e *big.Int, public *PublicPart) bool {
+	_x1 := big.NewInt(0).Exp(public.y, e, public.p)
+	_x2 := big.NewInt(0).Exp(public.g, s, public.p)
+	_x1.Mul(_x1, _x2)
+	_x1.Mod(_x1, public.p)
+
+	return x.Cmp(_x1) == 0
 }
--- bitcommit/schnorr_test.go
+++ bitcommit/schnorr_test.go
@@ -1,9 +1,50 @@
 package bitcommit
 
 import (
+	"math/big"
 	"testing"
 )
 
+// It works, but requires refactoring
+
 func TestSchnorrSignature(t *testing.T) {
-	Test()
+	ctx := &SchnorrContext{
+		q:  big.NewInt(0),
+		qp: big.NewInt(0),
+		p:  big.NewInt(0),
+		a:  big.NewInt(0),
+		g:  big.NewInt(0),
+		y:  big.NewInt(0),
+		w:  big.NewInt(0),
+	}
+
+	err := ctx.Init()
+	if err != nil {
+		t.Error("Context initialization has failed.")
+	}
+
+	//private := ctx.GetPrivatePart()
+	Public := ctx.GetPublicPart()
+
+	clientRandom, err := Public.FindR()
+	if err != nil {
+		t.Error("Failed to generate random values for client (r, x).")
+	}
+
+	// A sends x to B
+	x := clientRandom.x
+
+	// B sends random e
+	e, err := GenE()
+	if err != nil {
+		t.Error("failed to generate B's response (e).")
+	}
+
+	// A calculates: s = r + we (mod q) and sends it to B
+	s := ctx.FindS(e, clientRandom)
+
+	// B checks
+	if !IsApproved(x, s, e, Public) {
+		t.Error("Approve has failed. That is not supposed to happen")
+	}
 }
